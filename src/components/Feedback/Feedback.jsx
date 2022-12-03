7395aa29c7682a8b0152641093167c69997c59ed
Serhii Malychkovych <masevik@gmail.com>
2022-12-03 18:02:10 +0200

    finished

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,4 +1,6 @@
+import PropTypes from 'prop-types';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Notification } from 'components/Notification/Notification';
 import { Section } from 'components/Section/Section';
 import { Statistics } from 'components/Statistics/Statistics';
 import { Box } from '../Box';
@@ -15,19 +17,32 @@ export const Feedback = ({
     <Box pl="100px" pt="20px">
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={(good, neutral, bad)}
+          options={{ good, neutral, bad }}
           onLeaveFeedback={onClick}
         />
       </Section>
-      <Section title="Statistics">
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positivePercentage={positivePercentage}
-        />
-      </Section>
+      {total === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      )}
     </Box>
   );
+};
+
+Feedback.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { StatisticsItem } from './Statistics.styled';
 
 export const Statistics = ({
@@ -8,29 +9,35 @@ export const Statistics = ({
   positivePercentage,
 }) => {
   return (
-    <>
-      <ul>
-        <StatisticsItem>
-          <span>Good:</span>
-          <span>{good}</span>
-        </StatisticsItem>
-        <StatisticsItem>
-          <span>Neutral:</span>
-          <span>{neutral}</span>
-        </StatisticsItem>
-        <StatisticsItem>
-          <span>Bad:</span>
-          <span>{bad}</span>
-        </StatisticsItem>
-        <StatisticsItem>
-          <span>Total:</span>
-          <span>{total}</span>
-        </StatisticsItem>
-        <StatisticsItem>
-          <span>Positive feedback:</span>
-          <span>{positivePercentage}%</span>
-        </StatisticsItem>
-      </ul>
-    </>
+    <ul>
+      <StatisticsItem>
+        <span>Good:</span>
+        <span>{good}</span>
+      </StatisticsItem>
+      <StatisticsItem>
+        <span>Neutral:</span>
+        <span>{neutral}</span>
+      </StatisticsItem>
+      <StatisticsItem>
+        <span>Bad:</span>
+        <span>{bad}</span>
+      </StatisticsItem>
+      <StatisticsItem>
+        <span>Total:</span>
+        <span>{total}</span>
+      </StatisticsItem>
+      <StatisticsItem>
+        <span>Positive feedback:</span>
+        <span>{positivePercentage}%</span>
+      </StatisticsItem>
+    </ul>
   );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
